a535da07133706a2e52f9e9efe6b03e2107f7e92
Roger Best <rbest@microsoft.com>
2026-02-18 16:41:50 -0800

    Update to vs 2026

--- scripts/Install-VS2026.ps1
+++ scripts/Install-VS2026.ps1
@@ -2,18 +2,18 @@
 # Licensed under the MIT License.
 
 # Details:
-# - https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2022
-# - https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-enterprise?view=vs-2022
+# - https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2026
+# - https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-enterprise?view=vs-2026
 
 $ProgressPreference = 'SilentlyContinue'	# hide any progress output
 
 $installerName = "vs_enterprise.exe"
 $installerPath = Join-Path -Path $env:TEMP -ChildPath $installerName
 
-Write-Host "[${env:username}] Downloading VS2022 ..."
-(new-object net.webclient).DownloadFile('https://aka.ms/vs/17/release/vs_enterprise.exe', $installerPath)
+Write-Host "[${env:username}] Downloading VS2026 ..."
+(new-object net.webclient).DownloadFile('https://aka.ms/vs/19/release/vs_enterprise.exe', $installerPath)
 
-Write-Host "[${env:username}] Installing VS2022 ..."
+Write-Host "[${env:username}] Installing VS2026 ..."
 $process = Start-Process -FilePath $installerPath -ArgumentList `
 	"--add", "Microsoft.VisualStudio.Workload.CoreEditor", `
 	"--add", "Microsoft.VisualStudio.Workload.Azure", `
